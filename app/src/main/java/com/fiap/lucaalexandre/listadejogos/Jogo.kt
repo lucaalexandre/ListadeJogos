bdf38bb970909928158f7acc1f311370540335e4
logonrm <logonrm@fiap.com.br>
2018-05-04 09:46:04 -0300

    Initial commit

---
+++ app/src/main/java/com/fiap/lucaalexandre/listadejogos/Jogo.kt
@@ -0,0 +1,6 @@
+package com.fiap.lucaalexandre.listadejogos
+
+data class Jogo(val imagemId: Int,
+                val titulo: String,
+                val anoLancamento: Int,
+                val descricao: String)
---
+++ app/src/main/java/com/fiap/lucaalexandre/listadejogos/ListaActivity.kt
@@ -0,0 +1,12 @@
+package com.fiap.lucaalexandre.listadejogos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+    }
+}
